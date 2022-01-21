854833b25b086d67eca52aabe1ddb3c99eef6c67
afsal-parseltongue <90751398+afsal-parseltongue@users.noreply.github.com>
2022-01-21 15:27:10 +0530

    Cricket notification script

---
+++ cricinfo_notification.py
@@ -0,0 +1,54 @@
+
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+
+
+
+
+def get_score_and_notify():
+    url = "https://www.espncricinfo.com/series/zimbabwe-in-sri-lanka-2021-22-1294726/sri-lanka-vs-zimbabwe-2nd-odi-1294970/live-cricket-score"
+    response  = requests.get(url)
+    soup = BeautifulSoup(response.content, "lxml")
+    score_parent = soup.find('div', attrs = {'class':'match-info match-info-MATCH match-info-MATCH-full-width'})
+
+    batting_team_div = score_parent.find("div", attrs={"class": "team"})
+    name_div = batting_team_div.find('p', attrs = {'class': 'name'})
+    overs_div = batting_team_div.find('span', attrs={"class": 'score-info'})
+    score_div = batting_team_div.find_all('span', attrs={"class": "score"})
+
+
+    batting_team_name = name_div.text
+    display_text = ""
+    if batting_team_name:
+        display_text += batting_team_name
+        display_text += " "
+    overs_text = overs_div.text
+    if overs_text:
+        display_text += overs_text
+        display_text += " "
+
+    scores = []
+    for span in score_div:
+        scores.append(span.text.replace("\xa0", " "))
+
+
+
+    scores_string = "".join(scores)
+
+    if scores_string:
+        display_text += scores_string
+
+
+    cmd = f'/usr/bin/notify-send "Cricket Score" "{display_text}"'
+    os.system(cmd)
+
+
+starttime = time.time()
+while True:
+    print("tick")
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+    get_score_and_notify()
+
+
